067b7d74c3c223bcaf58bc83c9c1bd5bd8482280
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-11 13:21:39 -0500

    Update supabase url and key to point to my project

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_URL = 'https://nxtckliabtixkexceffw.supabase.co';
 const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54dGNrbGlhYnRpeGtleGNlZmZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk2Mzg5MTgsImV4cCI6MTk3NTIxNDkxOH0.cjq0XpKdcTVMbEd38x6tI54kqPmiTVNNM5zI8-p7k5c';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
