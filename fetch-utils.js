ed9303b68bdafcb7cea9362f0cac239d2d14fd7a
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-11 13:22:24 -0500

    Insert a single game into the games table

--- fetch-utils.js
+++ fetch-utils.js
@@ -20,7 +20,7 @@ The games are stored in the database using this data model:
 
 export async function createGame(game) {
     // create a single new game in the games table using the above object
-
+    const response = await client.from('games').insert(game).single();
     return checkError(response);
 }
 
